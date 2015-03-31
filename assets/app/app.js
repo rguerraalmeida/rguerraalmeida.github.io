5a95230701137698abebd29ee57f4740df83e12c
Rui Guerra Almeida <r.guerra.almeida@gmail.com>
2015-03-31 11:43:41 +0100

    Same as previous, minor bugfix

--- assets/app/app.js
+++ assets/app/app.js
@@ -5,18 +5,23 @@
 	var ENTER_KEY = 13;
 	var ESCAPE_KEY = 27;
 
-	if (!window.Router)
-		document.write("I didn't saw director around"));
+	if (!window.Router){
+		document.write("I didn't saw director around")); return;
+	}
 
-	if (!window.jQuery)
-		document.write("I didn't saw jQuery around"));
+	if (!window.jQuery){
+		document.write("I didn't saw jQuery around")); return;
+	}
 
-	if (!window.ko)
-		document.write("I didn't saw knockout around"));
+	if (!window.ko){
+		document.write("I didn't saw knockout around")); return;
+	}
+
+	document.write("all goes well")); return;
 
 	// bind a new instance of our view model to the page
-	var viewModel = new ViewModel(prescriptions || []);
-	ko.applyBindings(viewModel);	
+	/*var viewModel = new ViewModel(prescriptions || []);
+	ko.applyBindings(viewModel);	*/
 	// set up filter routing
 	/*jshint newcap:false */
 	Router({ '/:filter': viewModel.showMode }).init();
